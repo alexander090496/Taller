ba5fe0db1507ddcfd7bac3d45ac6ce77e98cd348
Alexander <jorgeAn09@hotmail.com>
2017-09-03 13:10:50 -0500

    fin

--- app/src/main/java/com/i015114/taller/MainActivity.java
+++ app/src/main/java/com/i015114/taller/MainActivity.java
@@ -1,7 +1,12 @@
 package com.i015114.taller;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton flo=(FloatingActionButton) findViewById(R.id.float_1);
+        registerForContextMenu(flo);
+
+
+    }
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        menu.setHeaderTitle("Mis opciones");
+        inflater.inflate(R.menu.menu1, menu);
+
     }
 }
